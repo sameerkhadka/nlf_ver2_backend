1134a01930f960a8820abec2b530e4e7764b18b1
Yogesh Karki <77563612+yogesh-karki@users.noreply.github.com>
2024-01-16 13:47:00 +0545

    image landscape

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -164,7 +164,7 @@
         {!!$item->about!!}
       </div>
       <div class="row justify-content-md-center">
-        <div class="col-md-6">
+        <div class="col-md-12">
           <div class="img">
             <img src="{{Voyager::image($item->about_img1)}}" alt="" />
           </div>
